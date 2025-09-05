cad9603aee8f44893a9e7cc8a652762a7be64144
Fernando Navarro <fernandonavarro@berkeley.edu>
2025-09-05 15:19:38 -0700

    Mobile h-screen fix

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -45,7 +45,7 @@ export default async function HomePage() {
   const projects = await client.fetch(queryProjects);
 
   return (
-    <div className="z-0 h-screen snap-y snap-mandatory overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar overflow-x-hidden scrollbar-track-gray-400/20 scrollbar-thumb-[#ff25e2]/20">
+    <div className="z-0 h-[100svh] pb-[env(safe-area-inset-bottom)] snap-y snap-mandatory overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar overflow-x-hidden scrollbar-track-gray-400/20 scrollbar-thumb-[#ff25e2]/20">
       <Header socials={socials} />
 
       <section id="hero" className="snap-center">
